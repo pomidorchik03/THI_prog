2950e74cf86c510a342bd250af6642423ea3b620
pomidorchik03 <london-266@mail.ru>
2026-03-08 22:13:17 +0700

    Initial commit

--- includes/SetRoutes.hpp
+++ includes/SetRoutes.hpp
@@ -34,7 +34,7 @@ public:
         int totalPrice = 0;
         for (ITransport* transport : transports_)
         {
-            totalPrice += transport->GetPrice();
+            totalPrice += transport->GetPrice() * 2;
         }
         return totalPrice;
     }
